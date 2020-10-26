59829e3374d54ae91525b48f776c8c2de3a1620c
AdamJudge <adamjudge2406@gmail.com>
2020-10-26 17:05:11 +0000

    Started work on actions

---
+++ src/mechanics/actions/Move.java
@@ -0,0 +1,14 @@
+package mechanics.actions;
+
+/* 
+	Check pawns possible return places (TO BE DONE IN PAWN)
+	
+	MoveTo()
+*/
+
+public class Move extends Action{
+	@Override
+	public void doAction() {
+		// Move pawn to elem of array list
+	}
+}
---
+++ src/mechanics/actions/ShoreUp.java
@@ -0,0 +1,11 @@
+package mechanics.actions;
+
+public class ShoreUp  extends Action{
+
+
+	@Override
+	public void doAction() {
+		//ShoreUp elem of arraylist!
+	}
+
+}
