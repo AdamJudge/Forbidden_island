062017c0c402bd4f5f44b3567934dc8bd861f007
catherine-waechter <catherine.waechter@gmail.com>
2020-11-09 17:16:58 +0000

    Added code where we had placeholders

--- src/elements/pawns/Explorer.java
+++ src/elements/pawns/Explorer.java
@@ -1,10 +1,64 @@
 package elements.pawns;
 
-public class Explorer extends Pawn {
+import java.util.Set;
 
-	// override 
-	// moveCheck
-	// shoreuCheck
-	// shoreup
+import elements.board.Tile;
+import elements.board.TileStatus;
+
+/**
+ * Explorer class
+ * 	Represents the explorer pawn (Green)
+ * 
+ * @author Catherine Waechter
+ * @version 1.0
+ * 
+ *  Date created: 26/10/20
+ *  Last modified: 09/11/20
+ */
+public class Explorer extends Pawn {
+	/**
+	 * moveCheck
+	 * 	based on the Pawn moveCheck, but adds diagonal tiles
+	 * @param allTiles
+	 * @return set of valid tiles
+	 */
+	@Override
+	public Set<Tile> moveCheck(Set<Tile> allTiles){
+		Set<Tile> validTiles = super.moveCheck(allTiles); // tiles that are valid for the base pawn are also valid for the explorer
+		
+		// add diagonal tiles to the set of valid tiles
+		for (Tile tile : allTiles) {
+			if(position.getY() + 1 == tile.getY()) {		// row of tiles below the pawn
+				if(position.getX() + 1 == tile.getX() || position.getX() - 1 == tile.getX()) { 	// diagonal tiles
+					validTiles.add(tile);
+				}
+			}
+			else if(position.getY() - 1 == tile.getY()) {		// row of tiles above the pawn
+				if(position.getX() + 1 == tile.getX() || position.getX() - 1 == tile.getX()) { 	// diagonal tiles
+					validTiles.add(tile);
+				}
+			}
+		}
+		
+		return validTiles;
+	}
+	
+	/**
+	 * shoreupCheck
+	 * 	shoreup rules and move rules are the same for the explorer, so use the move method
+	 * 		then check tile status.
+	 * @param allTiles
+	 * @return set of valid tiles
+	 */
+	@Override
+	public Set<Tile> shoreupCheck(Set<Tile> allTiles){			
+		Set<Tile> validTiles = moveCheck(allTiles);
+		for (Tile tile : validTiles) {
+			if (tile.getStatus() != TileStatus.FLOODED) {
+				tile.remove();
+			}
+		}
+		return validTiles;
+	}
 	
 }
--- src/elements/pawns/Navigator.java
+++ src/elements/pawns/Navigator.java
@@ -1,8 +1,43 @@
 package elements.pawns;
 
+import java.util.Set;
+
+import elements.board.Tile;
+
+
+/**
+ * Navigator class
+ * 	Represents the navigator pawn (Yellow)
+ * 
+ * @author Catherine Waechter
+ * @version 1.0
+ * 
+ *  Date created: 26/10/20
+ *  Last modified: 09/11/20
+ */
 public class Navigator extends Pawn {
 
-	// method:
-	// move_other(Pawn other_pawn, Tile destination) 
+	/**
+	 * moveOtherCheck
+	 * 	Returns set of tiles another pawn can move to (only adjacent tiles) 
+	 * ------------------------------------- NOTE: can do this twice for one action, but will need to be checked again after moving once ------------------!!
+	 * @param otherPawn - pawn we want the navigator to move
+	 * @param allTiles 	
+	 * @return set of valid tiles for the other pawn 
+	 */
+	public Set<Tile> moveOtherCheck(Pawn otherPawn, Set<Tile> allTiles) {
+		
+		Set<Tile> validTiles = super.moveCheck(allTiles); 	// only adjacent tiles, regardless of pawn type
+		return validTiles;
+	}
 	
+	/**
+	 * moveOther
+	 * 	moves given pawn to a destination tile
+	 * @param otherPawn
+	 * @param destination
+	 */
+	public void moveOther(Pawn otherPawn, Tile destination) {
+		otherPawn.move(destination);
+	}
 }
