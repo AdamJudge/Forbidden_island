ede237e3fe02e7128cf981edaf196bacb313d008
catherine-waechter <catherine.waechter@gmail.com>
2020-11-23 15:23:47 +0000

    Initial commit

---
+++ src/elements/cards/TreasureCardTypes.java
@@ -0,0 +1,19 @@
+package elements.cards;
+
+/**
+ * TreasureCardTypes enum
+ * 	Possible treasure card types
+ * 
+ * @author Catherine Waechter
+ * @version 1.0
+ * 
+ * Date Created: 23/10/20
+ * Last Modified: 23/11/20
+ *
+ */
+public enum TreasureCardTypes {
+	SANDBAGS,
+	HELICOPTER,
+	WATERSRISE,
+	TREASURE
+}
