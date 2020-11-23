a9adba3c243a3df9fdf9bf77b06518a8ba89d623
catherine-waechter <catherine.waechter@gmail.com>
2020-11-23 12:21:08 +0000

    Changed to singleton class, and added getAllTiles and getRemainingTiles methods

--- src/elements/board/Board.java
+++ src/elements/board/Board.java
@@ -11,13 +11,53 @@ import java.util.ArrayList;
  * Creates a board made up of Tile objects
  * 
  * @author Catherine Waechter
- * @version 1.0
+ * @version 2.0
+ * 	Board is now a singleton class
+ * 	added getAllTiles and getReminingTiles methods
+ * 
  * Date created: 26/10/20
- * Last modified: 29/10/20
+ * Last modified: 23/11/20
  */
 public class Board {
-	static private int nbr_tiles =24;	// number of tiles in the board
-	private Set<Tile> tiles;
+	private static Board board = null;
+	private Set<Tile> allTiles;
+	
+	/**
+	 * getAllTiles
+	 * 	returns all tiles (even if removed)
+	 * @return
+	 */
+	public Set<Tile> getAllTiles(){
+		return allTiles;
+	}
+	
+	/**
+	 * getRemainingTiles 
+	 * 	returns tiles that are still on the board (not removed)
+	 * @return 
+	 */
+	public Set<Tile> getRemainingTiles(){
+		Set<Tile> remainingTiles = new HashSet<Tile>();
+		for (Tile tile : allTiles) {
+			if(tile.getStatus() != TileStatus.REMOVED) {
+				remainingTiles.add(tile);
+			}
+		}
+		return remainingTiles;
+	}
+	
+	/**
+	 * getInstance
+	 * 	returns single instance of board
+	 * @return board
+	 */
+	public static Board getInstance() {
+		if(board == null) {
+			board = new Board();
+		}
+		return board;
+	}
+	
 	/**
 	 * Board Constructor
 	 * 	Creates all tiles in the board
@@ -26,7 +66,7 @@ public class Board {
 		//Set<Tile> tiles;			// set of tiles in the board
 		// NB: tiles can be a set because the order is determined by the position field of each tile
 		
-		this.tiles = new HashSet<Tile>(); // create empty set of tiles
+		this.allTiles = new HashSet<Tile>(); // create empty set of tiles
 		
 		ArrayList<Position> positions = new ArrayList<Position>(); // create empty list of positions
 		
@@ -44,7 +84,7 @@ public class Board {
 		
 		// create tiles : name is in a set order, but position is randomised
 		for (TileNames name : TileNames.values()) {
-			this.tiles.add(new Tile(name, positions.get(this.tiles.size())));
+			this.allTiles.add(new Tile(name, positions.get(this.allTiles.size())));
 		}
 	}
 }
