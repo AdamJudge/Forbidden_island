5f6a6c7ce39e42c8fb6b05413544db4a616e4df4
catherine-waechter <catherine.waechter@gmail.com>
2020-11-25 12:32:48 +0000

    replaced placeholders with code

--- src/elements/cards/DiscardPile.java
+++ src/elements/cards/DiscardPile.java
@@ -1,12 +1,45 @@
 package elements.cards;
 
-public class DiscardPile {
+import java.util.Collections;
+import java.util.Stack;
 
-	// fields
-	// ArrayList<Card> discardedCards
+/**
+ * DiscardPile class
+ * 	represents a discard pile
+ * 
+ * @author Catherine Waechter
+ * @version 1.0
+ * 
+ * Date created 26/10/20
+ * Last modified 25/11/20
+ *
+ */
+public abstract class DiscardPile {
+
+	protected Stack<Card> discardedCards;
 	
-	// methods
-	// void shuffle(void)
-	// void addToDeck(void)
+	
+	public abstract void toDeck();
+	
+	/**
+	 * returnToDeck
+	 * shuffles the discard pile and returns cards to a deck (general)
+	 */
+	public void returnToDeck(Deck deckInstance) {
+		Collections.shuffle(discardedCards);
+		int totalCards = discardedCards.size();
+		for(int i = 0; i<totalCards; i++) {
+			deckInstance.addCard(discardedCards.pop());
+		}
+	}
+	
+	/**
+	 * addCard
+	 * 	Adds a card to the discard pile
+	 * @param card
+	 */
+	public void addCard(Card card) {
+		discardedCards.add(card);
+	}
 	
 }
