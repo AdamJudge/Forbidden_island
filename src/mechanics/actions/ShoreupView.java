b555e70e1c2830c292ada4c407e3dcde0d14d64e
catherine-waechter <catherine.waechter@gmail.com>
2020-12-04 13:35:07 +0000

    Implement Shoreup as View

--- src/mechanics/actions/ShoreupView.java
+++ src/mechanics/actions/ShoreupView.java
@@ -17,36 +17,72 @@ import java.util.List;
 import java.util.Scanner;
 
 import elements.board.Tile;
-import elements.pawns.Explorer;
+import elements.pawns.Engineer;
 import elements.pawns.Pawn;
+import mechanics.TurnController;
+import players.Player;
 import setup.ParseNumberInputs;
 
-public class ShoreUp  extends Action{
+public class ShoreupView  extends ActionView{
 
+	private static ShoreupView shoreupView = null;
+	private TurnController controller;
+	
+	/**
+	 * getInstance
+	 * 	get singleton instance of ShoreupView
+	 * @param controller - controller associated with the view
+	 * @return shoreupView (singleton instance)
+	 */
+	public static ShoreupView getInstance(TurnController controller) {
+		if(shoreupView == null) { 
+			shoreupView = new ShoreupView(controller);
+		}
+		return shoreupView;
+	}
+	
+	/**
+	 * ShoreupView Constructor
+	 * @param controller	 - turn controller 
+	 */
+	private ShoreupView(TurnController controller) {
+		this.controller = controller;
+	}
 
-	public void shoreUp(Pawn pawn, Scanner user) throws IOException {
-		// Move pawn to elem of array list
-		Tile destTile;
-		int limit;
-		int userNum=-1;
-		List<Tile> possibleTiles = new ArrayList<Tile>();
-		if (pawn instanceof Explorer) {
-			//Override for explorer as method is different
-			possibleTiles=((Explorer)pawn).shoreupCheck();
-		} else {
-			possibleTiles=pawn.shoreupCheck();
-		}
+	public boolean doAction(Player player, Scanner user) throws IOException {
+		int limit, userNum;
 		
-		System.out.println("Which tile do you want to shore up? [0-9]");
-		limit = possibleTiles.size();
-		for (Tile t:possibleTiles) {
-			System.out.println(t.getName());
-		}
-		userNum=ParseNumberInputs.main(user, 0, limit);
+		for(int i = 0; i<2; i++) {
+			ArrayList<Tile> possibleTiles = controller.getShoreupCheck(player);
+			
+			limit = possibleTiles.size();
+			if(i == 0) {	// printout is different if there is another shore up
+			System.out.println("Which tile do you want to shore up? (enter 0 to cancel and pick another action)");
+			}
+			printTileList(possibleTiles);
 		
-		destTile=possibleTiles.get(userNum);
-		pawn.shoreup(destTile);
-		//validTiles=pawn.moveCheck();
+			userNum=ParseNumberInputs.main(user, 0, limit);
+			if(userNum == 0 && i == 0) {
+				return false;
+			}
+			else if(userNum == 0 && i == 1) {
+				break;
+			}
+			
+			Tile chosenTile = possibleTiles.get(userNum-1);
+			controller.shoreup(player, chosenTile);
+			
+			System.out.println("Tile : " + chosenTile + "Status: " + chosenTile.getStatus().name());
+			
+			if(player.getPawn() instanceof Engineer && i==0) {
+				System.out.println("You can shore up another tile for no additional actions!");
+				System.out.println("Which tile do you want to shore up? [1-9] (Enter 0 if you don't want to shore up another tile. Will still use an action for first shore-up)");
+			}
+			else {	// other pawns don't get a second shore up
+				break;
+			}
+		}	
+		return true;
 	}
 }
 
