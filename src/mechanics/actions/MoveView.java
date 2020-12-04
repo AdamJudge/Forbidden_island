380eb4f5d836e2c1c93e15e2b40f5fe910407577
catherine-waechter <catherine.waechter@gmail.com>
2020-12-04 13:34:46 +0000

    Implement Move as View

--- src/mechanics/actions/MoveView.java
+++ src/mechanics/actions/MoveView.java
@@ -13,13 +13,13 @@ package mechanics.actions;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
 
 import elements.board.Tile;
 import elements.pawns.*;
+import mechanics.TurnController;
+import players.Player;
+import players.PlayerList;
 import setup.ParseNumberInputs;
 
 /* 
@@ -28,29 +28,77 @@ import setup.ParseNumberInputs;
 	MoveTo()
 */
 
-public class Move extends Action{
-	public void movePawn(Pawn pawn, Scanner user) throws IOException {
-		// Move pawn to elem of array list
-		Tile destTile;
-		int limit;
-		int userNum=-1;
-		List<Tile> possibleTiles = new ArrayList<Tile>();
-		if (pawn instanceof Explorer) {
-			//Override for explorer as method is different
-			possibleTiles=((Explorer)pawn).moveCheck();
-		} else {
-			possibleTiles=pawn.moveCheck();
+public class MoveView extends ActionView{
+	
+	private Player playerToMove;
+	private Scanner user;
+	
+	private static MoveView moveView = null;
+	private TurnController controller;
+	
+	/**
+	 * getInstance
+	 * 	get singleton instance of MoveView
+	 * @param controller - controller associated with the view
+	 * @return moveView (singleton instance)
+	 */
+	public static MoveView getInstance(TurnController controller) {
+		if(moveView == null) { 
+			moveView = new MoveView(controller);
+		}
+		return moveView;
+	}
+	
+	/**
+	 * MoveView Constructor
+	 * @param controller	 - turn controller 
+	 */
+	private MoveView(TurnController controller) {
+		this.controller = controller;
+	}
+	
+	public boolean doAction(Player currentPlayer, Scanner user) throws IOException {
+		
+		this.user = user;
+		playerToMove = currentPlayer;
+	
+		// Navigator exception
+		if(currentPlayer.getPawn() instanceof Navigator) {
+			playerToMove = navigatorException(currentPlayer);
 		}
 		
-		System.out.println("Which tile do you want to move to? [0-9]");
-		limit = possibleTiles.size();
-		for (Tile t:possibleTiles) {
-			System.out.println(t.getName());
-		}
-		userNum=ParseNumberInputs.main(user, 0, limit);
+		// TODO Pilot exception
 		
-		destTile=possibleTiles.get(userNum);
-		pawn.move(destTile);
-		//validTiles=pawn.moveCheck();
+		System.out.println(playerToMove + " can move to ");
+		ArrayList<Tile> possibleTiles = controller.getMoveCheck(playerToMove);
+		int limit = possibleTiles.size();
+		System.out.println("Which tile do you want to move to? (Enter 0 to cancel and pick another action)");
+		printTileList(possibleTiles);
+		int userNum=ParseNumberInputs.main(user, 0, limit);
+		if(userNum == 0) {
+			return false;
+		}
+		Tile newTile = controller.move(playerToMove, possibleTiles.get(userNum-1));
+		System.out.println(playerToMove + " has moved to " + newTile);
+		
+		return true;
+	}
+	
+	private Player navigatorException(Player currentPlayer) throws IOException{
+		
+		System.out.println("Would you like to move ");
+		int i = 1;
+		for (Player player : PlayerList.getInstance().getPlayers()) {
+			if(player != currentPlayer) {
+				System.out.println("[" + i + "]" + player);
+			}
+			else {
+				System.out.println("[" + i + "]" + player + "(You)");
+			}
+			i++;
+		}
+		int input=ParseNumberInputs.main(user, 1, i);
+		return PlayerList.getInstance().getPlayers().get(input-1);
+	
 	}
 }
