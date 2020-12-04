36d8be3331baeffa1792ff7dd28549086fcb2b23
catherine-waechter <catherine.waechter@gmail.com>
2020-12-04 20:25:33 +0000

    Removed unused functions, added comments

--- src/mechanics/actions/ActionView.java
+++ src/mechanics/actions/ActionView.java
@@ -4,10 +4,11 @@
  * 	superclass for action views
  * 
  * Author: @author Adam Judge, Catherine Waechter
- * Version: @version 1.1
- * 	added doAction and printTileList
+ * Version: @version 1.2
+ * 	added printCardList 
+ * 
  * Creation Date: 22/10/20
- * Last Modified: 03/12/20
+ * Last Modified: 04/12/20
  */
 
 package mechanics.actions;
@@ -21,21 +22,15 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.IOException;
 
-/* Three abstract classes to be implemented by actions.
-	
-	Print all actions for user selection m/s/g/t
-	Check conditions/possible
-	Check conditions, i.e player location, prints list of actions to terminal (m/s/g/t) TO BE DONE IN THIS CLASS
-	do_action() //Takes in input and does action if possible TO BE DONE IN SUB CLASS
-
-*/
 public abstract class ActionView{
-	//protected ArrayList<String> actionList = new ArrayList<String>();
-	// Array list of actions
-	// Selected element in array list
 	
 	public abstract boolean doAction(Player currentPlayer, Scanner user) throws IOException;
 	
+	/**
+	 * printTileList 
+	 * 	print given list of tiles
+	 * @param tiles
+	 */
 	public void printTileList(ArrayList<Tile> tiles) {
 		int i = 1;
 		for (Tile tile :tiles) {
@@ -44,6 +39,11 @@ public abstract class ActionView{
 		}
 	}
 	
+	/**
+	 * printCardList
+	 *  prints given cards
+	 * @param cards
+	 */
 	public void printCardList(ArrayList<Card> cards) {
 		int i = 1;
 		for (Card card : cards) {
@@ -54,7 +54,7 @@ public abstract class ActionView{
 	
 	/**
 	 * printPlayerList 
-	 * 	print list of players, except currentPlayer
+	 * 	print list of players, highglight currentPlayer
 	 * @param players - list of players to be printed
 	 * @param currentPlayer
 	 */
@@ -70,15 +70,4 @@ public abstract class ActionView{
 			i++;
 		}
 	}
-	
-	public void getActions() {
-		// Will take an array list from something
-	}
-	
-	public int checkAction() {
-		return 0;
-		// Print array list, get user input, select action
-	}
-	
-	//public abstract void doAction();
 }
