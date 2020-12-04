58bfbcf420977946855e0a809257d6955152fcd3
catherine-waechter <catherine.waechter@gmail.com>
2020-12-04 13:34:22 +0000

    Edited to suit ActionView

--- src/mechanics/actions/ClaimTreasureView.java
+++ src/mechanics/actions/ClaimTreasureView.java
@@ -1,19 +1,54 @@
 /**
- * Class Name: ClaimTreasure
+ * Class Name: ClaimTreasureView
  *
- * DETAILS
+ * 	View to display action of claiming a treasure
  * 
- * Author: @author adamj
- * Version: @version 
+ * @author Adam Judge, Catherine Waechter
+ * 
+ * @version 1.2
+ * 	adjusted to suit ActionView
+ * 
  * Creation Date: 22/10/20
- * Last Modified: 29/10/20
+ * Last Modified: 03/12/20
  */
 
 package mechanics.actions;
 
-public class ClaimTreasure extends Action{
+import players.Player;
+import mechanics.TurnController;
 
-	public void doAction() {
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class ClaimTreasureView extends ActionView{
+
+	private static ClaimTreasureView claimTreasureView = null;
+	private TurnController controller;
+	
+	public boolean doAction(Player currentPlayer, Scanner user) throws IOException {
 		// Claim treasure and GIVE THE PLAYER THE TREASURE
+		return true;
+	}
+	
+	/**
+	 * getInstance
+	 * 	get singleton instance of ClaimTreasureView
+	 * @param controller - controller associated with the view
+	 * @return claimTreasureView (singleton instance)
+	 */
+	public static ClaimTreasureView getInstance(TurnController controller) {
+		if(claimTreasureView == null) { 
+			claimTreasureView = new ClaimTreasureView(controller);
+		}
+		return claimTreasureView;
+	}
+	
+	/**
+	 * ClaimTreasureView Constructor
+	 * @param controller	 - turn controller 
+	 */
+	private ClaimTreasureView(TurnController controller) {
+		this.controller = controller;
 	}
 }
--- src/mechanics/actions/GiveCardView.java
+++ src/mechanics/actions/GiveCardView.java
@@ -1,21 +1,54 @@
 /**
- * Class Name: GiveCard
+ * Class Name: GiveCardView
  *
- * DETAILS
+ *  View to display action of giving a card
  * 
- * Author: @author adamj
- * Version: @version 
+ * @author Adam Judge, Catherine Waechter
+ * @version 1.2
+ * 	adjusted to suit ActionView 
+ * 
  * Creation Date: 22/10/20
- * Last Modified: 29/10/20
+ * Last Modified: 04/12/20
  */
 
 package mechanics.actions;
 
-public class GiveCard  extends Action{
+import players.Player;
+import mechanics.TurnController;
 
+import java.io.IOException;
+import java.util.Scanner;
 
-	public void doAction() {
+public class GiveCardView  extends ActionView{
+
+	private static GiveCardView giveCardView = null;
+	private TurnController controller;
+
+	public boolean doAction(Player currentPlayer, Scanner user) throws IOException {
 		// give elem of array list (person) item
+		return true;
 	}
 
+	
+	/**
+	 * getInstance
+	 * 	get singleton instance of GiveCardView
+	 * @param controller - controller associated with the view
+	 * @return giveCardView (singleton instance)
+	 */
+	public static GiveCardView getInstance(TurnController controller) {
+		if(giveCardView == null) { 
+			giveCardView = new GiveCardView(controller);
+		}
+		return giveCardView;
+	}
+	
+	/**
+	 * GiveCardView Constructor
+	 * @param controller	 - turn controller 
+	 */
+	private GiveCardView(TurnController controller) {
+		this.controller = controller;
+	}
+	
 }
