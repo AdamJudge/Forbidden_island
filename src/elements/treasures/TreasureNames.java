5b31fbf15742c2bd76e9df9f08b0c130a0c2d9c5
catherine-waechter <catherine.waechter@gmail.com>
2020-12-08 15:31:02 +0000

    Removed NONE type

--- src/elements/treasures/TreasureNames.java
+++ src/elements/treasures/TreasureNames.java
@@ -5,14 +5,15 @@ package elements.treasures;
  * list of names of treasures
  * 
  * @author Catherine Waechter
- * @version 1.0
+ * @version 2.0
+ * 	Removed "NONE" type
+ * 
  * Date created : 29/10/20
- * Last modified: 29/10/20
+ * Last modified: 08/12/20
  */
 public enum TreasureNames {
 	OCEAN_CHALICE,
 	CRYSTAL_OF_FIRE,
 	STATUE_OF_THE_WIND,
 	EARTH_STONE,
-	NONE 		// needed for tiles with no treasure associated
 }
