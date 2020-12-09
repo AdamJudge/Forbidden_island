d5eb229c4ad44036ec2c27411d4cb87e2065198f
AdamJudge <adamjudge2406@gmail.com>
2020-12-09 11:14:15 +0000

    Added ability to play anyones card during anyones turn

--- src/mechanics/actions/PlayCardView.java
+++ src/mechanics/actions/PlayCardView.java
@@ -13,15 +13,21 @@ package mechanics.actions;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 import elements.board.Tile;
 import elements.cards.Card;
 import elements.cards.TreasureCard;
+import elements.cards.TreasureCardTypes;
 import elements.pawns.Engineer;
 import elements.pawns.Pawn;
 import mechanics.TurnController;
+import mechanics.cardActions.Helicopter;
+import mechanics.cardActions.Sandbags;
 import players.Player;
 import setup.ParseNumberInputs;
 
@@ -53,18 +59,36 @@ public class PlayCardView  extends ActionView{
 
 	public boolean doAction(Player player, Scanner user) throws IOException {
 		int iter=0;
+		int iter2=0;
+		Map<Integer, Integer> handNumConv = new HashMap<Integer, Integer>();
+		String cardName;
 		System.out.println("Which card do you want to play " + player.getName() +"? (0 to cancel)");
+		String helicopter = TreasureCardTypes.HELICOPTER.toString();
+		String sandbags = TreasureCardTypes.SANDBAGS.toString();
 		for (Card c: player.getHand().getCards()) {
+			iter2+=1;
 			if( c instanceof TreasureCard) { //It always is but to be safe
-				//if () {
-					System.out.println(((TreasureCard)c).toString());
+				cardName=((TreasureCard)c).toString();
+				if (cardName.equals(helicopter) || cardName.equals(sandbags)) {
 					iter+=1;
+					handNumConv.put(iter, iter2);
 					System.out.println("["+iter+"]: " + c.toString());
-				//}
+				}
 			}
 		}
 		int cardNum = ParseNumberInputs.main(user, 0, iter);
-		//Card cardToPlay = player.getHand().getCards().;
+		if (cardNum == 0){
+			//Cancel
+			return false;
+		}
+		Card cardToPlay = player.getHand().getCards().get(handNumConv.get(cardNum)-1);
+		System.out.println("Card picked: "+cardToPlay.toString());
+		player.getHand().takeCard(cardToPlay);
+		if (cardToPlay.toString().equals(helicopter)) {
+			Helicopter.play(cardToPlay, player, user);
+		} else if (cardToPlay.toString().equals(sandbags)) {
+			Sandbags.play(cardToPlay, user);
+		}
 		return false;
 	}
 }
