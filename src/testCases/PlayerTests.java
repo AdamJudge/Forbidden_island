cf7183f5b26bbe7305079e17cdf46290678447a0
catherine-waechter <catherine.waechter@gmail.com>
2020-12-16 16:50:36 +0000

    Hand size test created

--- src/testCases/PlayerTests.java
+++ src/testCases/PlayerTests.java
@@ -2,15 +2,31 @@ package testCases;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.After;
 
 import elements.cards.TreasureDeck;
+import mechanics.TurnController;
+import mechanics.TurnView;
 import players.*;
 
 public class PlayerTests {
 
+	@Before 
+	public void testSetup() {
+		TurnController.getInstance();
+		TurnView view = TurnView.getInstance();
+		TurnController controller = TurnController.getInstance();
+		controller.setView(view);
+		view.setupView(controller);
+	}
+	
 	@Test
 	public void fullHandtest() throws IOException {
 		
@@ -18,11 +34,19 @@ public class PlayerTests {
 		
 		Player adam = PlayerList.getInstance().getPlayers().get(0);
 		
+		String input = "1";
+		InputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+		Scanner scanner = new Scanner(in);
+		
+		TurnView.getInstance().setScanner(scanner);
+		
 		for(int i=0; i<6; i++) {
 			adam.getHand().addCard(TreasureDeck.getInstance().draw());
 		}
 		
-		assertNotEquals("Hand should not exceed 5", 6, adam.getHand().getCards().size());
+		assertEquals("Hand should not exceed 5", 5, adam.getHand().getCards().size());
 	}
 
+	
 }
