d9ea0c4a9841147df40b84db1b19167f58dc052a
AdamJudge <adamjudge2406@gmail.com>
2020-12-21 17:46:09 +0000

    Removed system.exit(0)

--- src/mechanics/GameOver.java
+++ src/mechanics/GameOver.java
@@ -1,23 +1,22 @@
 package mechanics;
 
-public class GameOver{
-	private String resultString;
-	
-	public GameOver(Boolean gameVictory) {
+public class GameOver{	
+	public GameOver(Boolean gameVictory){
 		endGame(gameVictory);
 	}
 	
 	private void endGame(Boolean gameVictory) {
 		//If win else lose
+		String resultString;
 		if (gameVictory) {
 			resultString="You have escaped Forbidden Island!\n\nCongratulations!";
 		}	else {
 			resultString="You have failed to escaped Forbidden Island!\n\nBetter luck next time!";
 		}
-		printMessageAndExit();
+		printMessageAndExit(resultString);
 	}
 
-	private void printMessageAndExit() {
+	private void printMessageAndExit(String resultString){
 		System.out.println("   ____      _      __  __  U _____ u       U  ___ u__     __ U _____ u   ____     ");
 		System.out.println("U /\"___|uU  /\"\\  uU|' \\/ '|u\\| ___\"|/        \\/\"_ \\/\\ \\   /\"/u\\| ___\"|/U |  _\"\\ u  ");
 		System.out.println("\\| |  _ / \\/ _ \\/ \\| |\\/| |/ |  _|\"          | | | | \\ \\ / //  |  _|\"   \\| |_) |/");
@@ -26,6 +25,6 @@ public class GameOver{
 		System.out.println("  _)(|_   \\\\    >><<,-,,-.   <<   >>           \\\\     //       <<   >>   //   \\\\_");
 		System.out.println(" (__)__) (__)  (__)(./  \\.) (__) (__)         (__)   (__)     (__) (__) (__)  (__)\n");
 		System.out.println(resultString);
-		System.exit(0);
+		GamePlay.getInstance().setGameOver(true);
 	}
 }
