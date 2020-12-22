50634b58ad7e1e65473dc2d6d893931ef4af038a
catherine-waechter <catherine.waechter@gmail.com>
2020-12-22 12:30:49 +0000

    Adjusted for change in Setup

--- src/main/ForbiddenIsland.java
+++ src/main/ForbiddenIsland.java
@@ -1,29 +1,37 @@
 /**
- * Class Name:
+ * ForbiddenIsland 
  *
- * DETAILS
+ * Main class to run the game
  * 
- * Author: @author adamj
- * Version: @version 
+ * @author Adam Judge
+ * @version 1.2
+ * 
  * Creation Date: 22/10/20
  * Last Modified: 29/10/20
  */
 
 package main;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 import mechanics.GamePlay;
 import mechanics.setup.Setup;
 
 public class ForbiddenIsland {
+	
+	/**
+	 * main
+	 * 	
+	 * 	Carry out setup, and set game to play
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Scanner inScan = new Scanner(System.in);
-		Setup.getInstance().setupAll(inScan);
+		Setup.setupAndRun(inScan);		
 		
 		GamePlay.getInstance().playGame(inScan);
-		System.out.println("Thanks for Playing!");
+		System.out.println("Thanks for Playing!");		// TODO Remove when we've figured out all win / lose conditions ? 
         inScan.close();
 	}
 }
