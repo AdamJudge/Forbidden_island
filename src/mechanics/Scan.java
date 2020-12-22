0b09f9e14299f1a9b57251f3a0994ff3698c0bcc
catherine-waechter <catherine.waechter@gmail.com>
2020-12-22 15:15:41 +0000

    Initial commit. Singleton class to hold Scanner

---
+++ src/mechanics/Scan.java
@@ -0,0 +1,35 @@
+package mechanics;
+
+import java.util.Scanner;
+
+public class Scan {
+	private static Scan scan = null;
+	
+	private Scanner scanner;
+	
+	public void setScanner(Scanner input) {
+		scanner = input;
+	}
+	
+	public void close() {
+		scanner.close();
+	}
+	
+	public Scanner getScanner() {
+		return scanner;
+	}
+	
+	 /**
+	 * getInstance
+	 * @return setupView - singleton instance of Scan
+	 */
+	public static Scan getInstance() {
+		if(scan == null)
+			scan= new Scan();
+		return scan;
+	}
+	
+	private Scan() {
+		scanner = new Scanner(System.in);
+	}
+}
