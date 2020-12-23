f0a273e8edac4c2c69ab157b7db69ac8daae5b9f
catherine-waechter <catherine.waechter@gmail.com>
2020-12-23 09:58:44 +0000

    Moved helicopter methods to HelicopterController

--- src/mechanics/cardActions/CardActionController.java
+++ src/mechanics/cardActions/CardActionController.java
@@ -20,56 +20,11 @@ public class CardActionController {
 	private static CardActionController cardController = null;
 	private ActionController actionController;
 	
-	/**
-	 * getInstance
-	 * 	get singleton instance of CardActionController
-	 * @return cardController (singleton instance)
-	 */
-	public static CardActionController getInstance() {
-		if(cardController == null) { 
-			cardController = new CardActionController();
-		}
-		return cardController;
-	}
 	
-	public boolean tryLeave() {
-		GamePlay.getInstance().tryLeave();
-		//If unable to leave
-		if (!GamePlay.getInstance().canLeave()) {
-			return false;
-		}
-		return true;
-	}
-	
-	public boolean pilotFlown(Player player) {
-		Pawn pawn = player.getPawn();
-		if(pawn instanceof Pilot) {
-			return ((Pilot)pawn).getHasFlown();
-		}
-		else return false;
-	}
-	
-	public void pilotFixFlown(Player player, boolean flown) {
-		Pawn pawn = player.getPawn();
-		if(!flown && pawn instanceof Pilot) {
-			((Pilot)pawn).resetHasFlown();
-		}
-	}
-	
-	public ArrayList<Tile> getHelicopterTiles(Player player){
-		Pawn pawn = player.getPawn();
-		
-		Set<Tile> remainingTiles = Board.getInstance().getRemainingTiles();
-		
-		ArrayList<Tile> sortedTiles = new ArrayList<Tile>();
-		sortedTiles.addAll(remainingTiles);
-		sortedTiles.remove(pawn.getTile());	// pawn can't fly to its current tile
-		return sortedTiles;
-	}
 	
 	/**
 	 * getFloodedTiles
-	 * 	returns list of tiles that can be shored up
+	 * 	returns list of tiles that can be shored up	 (For Sandbags View)
 	 * @return floodedTiles
 	 */
 	public ArrayList<Tile> getFloodedTiles(){
@@ -86,6 +41,9 @@ public class CardActionController {
 	
 	/**
 	 * floodTile
+	 * 
+	 * 	Called by flood cards
+	 * 
 	 * 	if tile is normal, flood the tile and add card to discard pile
 	 * 	if tile is flooded, remove tile, and remove card (ie don't add to discard)
 	 * @param card
@@ -94,14 +52,10 @@ public class CardActionController {
 		
 		Tile tile = card.getTile();
 		
-		
 		if(tile.getStatus() == TileStatus.NORMAL) {
 			tile.flood();
 			FloodDiscard.getInstance().addCard(card);
 		}
-		
-		
-		
 		else if(tile.getStatus() == TileStatus.FLOODED) {
 			
 			
@@ -122,5 +76,16 @@ public class CardActionController {
 		this.actionController = actionController;
 	}
 	
+	/**
+	 * getInstance
+	 * 	get singleton instance of CardActionController
+	 * @return cardController (singleton instance)
+	 */
+	public static CardActionController getInstance() {
+		if(cardController == null) { 
+			cardController = new CardActionController();
+		}
+		return cardController;
+	}
 	
 }
