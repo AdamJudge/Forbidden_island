1dc9d6885985bd25466484694c41e4845e282f13
catherine-waechter <catherine.waechter@gmail.com>
2020-12-23 19:36:22 +0000

    Made methods static

--- src/mechanics/GameOver.java
+++ src/mechanics/GameOver.java
@@ -1,24 +1,43 @@
 package mechanics;
 
+/**
+ * GameOver
+ * 
+ * Display for game over. Created once at the end of the game.
+ * 
+ * @author Adam Judge, Catherine Waechter
+ * @version 1.4
+ * 	Made methods static
+ * 
+ * Date Created : 10/12/20
+ * Last Modified: 23/12/20
+ *
+ */
 public class GameOver{	
-	public GameOver(boolean gameVictory){
-		endGame(gameVictory);
-	}
 	
-	private void endGame(boolean gameVictory) {
-		//If win else lose
-		String resultString;
+	/**
+	 * endGame
+	 * 
+	 * prints end of game screen and sets "Game Over" in game play to true
+	 * 
+	 * @param gameVictory - true if win, false if lose
+	 */
+	public static void endGame(boolean gameVictory) {
+		
 		if (gameVictory) {
-			resultString="You have escaped Forbidden Island!\n\nCongratulations!";
-			printWin(resultString);
-		}	else {
-			resultString="You have failed to escaped Forbidden Island!\n\nBetter luck next time!";
-			printLoss(resultString);
+			printWin();
+		}	
+		else {
+			printLoss();
 		}
 		GamePlay.getInstance().setGameOver(true);
 	}
 
-	private void printWin(String resultString){
+	/**
+	 * printWin
+	 * print win screen
+	 */
+	private static void printWin(){
 		System.out.println("  __   __   U  ___ u   _   _                                _   _     _    ");
 		System.out.println("  \\ \\ / /    \\/\"_ \\/U |\"|u| |     __        __     ___     | \\ |\"|  U|\"|u");
 		System.out.println("   \\ V /     | | | | \\| |\\| |     \\\"\\      /\"/    |_\"_|   <|  \\| |> \\| |/");
@@ -26,10 +45,14 @@ public class GameOver{
 		System.out.println("    |_|   \\_)-\\___/  <<\\___/     U  \\ V  V /  U  U/| |\\u   |_| \\_|   (_)");
 		System.out.println(".-,//|(_       \\\\   (__) )(      .-,_\\ /\\ /_,-.-,_|___|_,-.||   \\\\,-.|||_");
 		System.out.println(" \\_) (__)     (__)      (__)      \\_)-'  '-(_/ \\_)-' '-(_/ (_\")  (_/(__)_)\n");
-		System.out.println(resultString);
+		System.out.println("You have escaped Forbidden Island!\n\nCongratulations!");
 	}
 	
-	private void printLoss(String resultString){
+	/**
+	 * printLoss
+	 * print loss screen
+	 */
+	private static void printLoss(){
 		System.out.println("   ____      _      __  __  U _____ u       U  ___ u__     __ U _____ u   ____     ");
 		System.out.println("U /\"___|uU  /\"\\  uU|' \\/ '|u\\| ___\"|/        \\/\"_ \\/\\ \\   /\"/u\\| ___\"|/U |  _\"\\ u  ");
 		System.out.println("\\| |  _ / \\/ _ \\/ \\| |\\/| |/ |  _|\"          | | | | \\ \\ / //  |  _|\"   \\| |_) |/");
@@ -37,6 +60,6 @@ public class GameOver{
 		System.out.println("  \\____| /_/   \\_\\ |_|  |_|  |_____|      \\_)-\\___/ U  \\_/-(_/ |_____|   |_| \\_\\");
 		System.out.println("  _)(|_   \\\\    >><<,-,,-.   <<   >>           \\\\     //       <<   >>   //   \\\\_");
 		System.out.println(" (__)__) (__)  (__)(./  \\.) (__) (__)         (__)   (__)     (__) (__) (__)  (__)\n");
-		System.out.println(resultString);
+		System.out.println("You have failed to escaped Forbidden Island!\n\nBetter luck next time!");
 	}
 }
