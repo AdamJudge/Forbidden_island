2bbe6ec417f471a1a4cd5be909e1606841d26d80
catherine-waechter <catherine.waechter@gmail.com>
2020-12-23 09:29:45 +0000

    Added tile flooding method that used to be in FloodTileView

--- src/mechanics/cardActions/CardActionController.java
+++ src/mechanics/cardActions/CardActionController.java
@@ -6,15 +6,19 @@ import java.util.Set;
 import elements.board.Board;
 import elements.board.Tile;
 import elements.board.TileStatus;
+import elements.cards.FloodCard;
+import elements.cards.FloodDiscard;
 import elements.pawns.Pawn;
 import elements.pawns.Pilot;
 import mechanics.GamePlay;
+import mechanics.actions.ActionController;
 import players.Player;
+import players.PlayerList;
 
 public class CardActionController {
 	
-	static CardActionController cardController = null;
-	
+	private static CardActionController cardController = null;
+	private ActionController actionController;
 	
 	/**
 	 * getInstance
@@ -78,6 +82,44 @@ public class CardActionController {
 		}
 		
 		return floodedTiles;
+	}
+	
+	/**
+	 * floodTile
+	 * 	if tile is normal, flood the tile and add card to discard pile
+	 * 	if tile is flooded, remove tile, and remove card (ie don't add to discard)
+	 * @param card
+	 */
+	public void floodTile(FloodCard card) {
+		
+		Tile tile = card.getTile();
+		
+		
+		if(tile.getStatus() == TileStatus.NORMAL) {
+			tile.flood();
+			FloodDiscard.getInstance().addCard(card);
+		}
+		
+		
+		
+		else if(tile.getStatus() == TileStatus.FLOODED) {
+			
+			
+			tile.remove();
+			ArrayList<Player> playersToSwim = new ArrayList<Player>();
+			for (Player player : PlayerList.getInstance().getPlayers()) {
+				if(player.getPawn().getTile() == tile) {
+					playersToSwim.add(player);
+				}
+			}
+			if(playersToSwim.size() != 0) {
+				actionController.doSwim(playersToSwim);
+			}
+		}
+	}
+	
+	public void setup(ActionController actionController) {
+		this.actionController = actionController;
 	}
 	
 	
